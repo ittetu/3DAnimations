496ca607230f8c6b847d2a0c6e97b5a91355c8ab
ittetu <tmeiadiusikadftuu@gmail.com>
2020-10-04 11:59:00 +0900

    motion okasii

--- Assets/Script/Runanimation.cs
+++ Assets/Script/Runanimation.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Runanimation : MonoBehaviour
 {
     float runSpeed = 10f;　//移動速度
-    float moveAgility = 500f;　
-    float jumpForce = 600f;
+    float moveAgility = 500f;
+    float jumpForce = 20f;
     float horizontalInput = 0f;
     float varticalInput = 0f;
     Vector3 difference;
-    Vector3 latestPos;　//前回位置
+    Vector3 differencexz;
+    Vector3 latestPos;
+    Vector3 rbVelo;
     Animator animator;
     Rigidbody rb;
 
@@ -27,9 +29,10 @@ public class Runanimation : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");　//
         varticalInput = Input.GetAxis("Vertical");//
 
+        _Update();
     }
 
-    void FixedUpdate()
+    void _Update()
     {
         Vector3 moceVector = Vector3.zero;
 
@@ -44,10 +47,11 @@ public class Runanimation : MonoBehaviour
         moceVector.x = runSpeed * horizontalInput;
         moceVector.z = runSpeed * varticalInput;
         //減速
-        rb.AddForce(moveAgility * (moceVector - rb.velocity));
+        rbVelo = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.AddForce(moveAgility * (moceVector - rbVelo));
 
         //ジャンプ
-        if (Input.GetKey(KeyCode.Space) && transform.position.y < 0.1) //ジャンプ
+        if (Input.GetKey(KeyCode.Space) && transform.position.y < 0.1)
         {
             animator.SetTrigger("jumpTrigger");
             rb.AddForce(transform.up * jumpForce,ForceMode.Impulse);
@@ -55,16 +59,17 @@ public class Runanimation : MonoBehaviour
 
         //移動距離
         difference = this.transform.position - latestPos;
-        latestPos = transform.position;
+        
+        differencexz = new Vector3(difference.x, 0, difference.z);
         //移動距離によって遷移
-        if (difference.magnitude > 0.03f)
+        Debug.Log(rb.velocity.magnitude);
+        if (rb.velocity.magnitude > 0.001f)
         {
             this.animator.SetBool("idleBool", false);　//走り状態遷移
-            if(rb.velocity.y < 0.01f)
-            {
-                transform.rotation = Quaternion.LookRotation(difference); //移動方向に向く
-            }
+            transform.rotation = Quaternion.LookRotation(differencexz); //移動方向に向く
+            
         }
+        latestPos = transform.position;
 
     }
 }
