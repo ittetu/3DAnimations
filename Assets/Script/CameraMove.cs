a2549cdabfc7db6e084c5546ebdaf53e832bf879
ittetu <tmeiadiusikadftuu@gmail.com>
2020-10-08 12:05:03 +0900

    カメラ追従 カメラ操作はI上J左K下L右で設定。上限なし プレイヤーを軸にしてカメラの視点が変わる
    プレイヤー操作もカメラの向きに沿って動くように変更。
    コードが微妙　あと、もしカメラをちょうど真上に向けるごとができたらプレイヤーの動きがおかしくなるかも。
    
    ジャンプのバグは未修正。
    着地をpysics.Raycastでしてみたが、着地寸前にジャンプボタンを押すと、Addforceが落下と打ち消しあって上に飛ばず、それ以降着地(Raycast)がtrueにならないか何かで処理が止まる。

---
+++ Assets/Script/CameraMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    GameObject player;
+    Vector3 playerMove = Vector3.zero;
+    Vector3 playerPos = Vector3.zero;
+    float rotateSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerMove = player.transform.position - playerPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キー操作でカメラ方向転換
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.RotateAround(player.transform.position, this.transform.right, -1 * rotateSpeed);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.RotateAround(player.transform.position, player.transform.up,rotateSpeed);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            transform.RotateAround(player.transform.position, player.transform.up, -1 * rotateSpeed);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.RotateAround(player.transform.position, this.transform.right, rotateSpeed);
+        }
+
+        //カメラを方向転換なしで追従
+        playerMove = player.transform.position - playerPos;
+        playerPos = player.transform.position;
+        this.transform.position += playerMove;
+    }
+}
---
+++ Assets/Script/JumpMove.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpMove : MonoBehaviour
+{
+    bool jumpSwitch = false;
+    bool jumping = false;
+    float jumpForce = 350f;
+    Rigidbody rb;
+    Animator animator;
+
+    void DoJump()//ジャンプ処理
+    {
+        animator.SetTrigger("jumpTrigger");
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ジャンプ操作
+        if (Input.GetKeyDown(KeyCode.Space) && jumpSwitch == false && jumping == false)
+        {
+            jumpSwitch = true;
+        }
+        //Debug.Log(jumpSwitch);
+        //Debug.Log(jumping);
+
+        if (Physics.Raycast(this.transform.position + new Vector3(0f,0.01f,0f), this.transform.TransformDirection(Vector3.down), 0.05f))
+        {
+            jumping = false;
+            animator.SetBool("groundBool", true);
+            Debug.Log("着地");
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        //ジャンプ判断
+        if(jumpSwitch == true && jumping == false)
+        {
+            DoJump();
+            Debug.Log("離陸");
+            jumpSwitch = false;
+            jumping = true;
+            animator.SetBool("groundBool", false);
+        }
+
+        //接地判定
+        /*if (Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.down), 0.1f))
+        {
+            jumping = false;
+            animator.SetBool("groundBool", true);
+            Debug.Log("着地");
+        }*/
+    }
+}
