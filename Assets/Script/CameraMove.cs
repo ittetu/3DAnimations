ef8e3195f55e82f0f45fa1fbd846555ad9c33dc6
ittetu <tmeiadiusikadftuu@gmail.com>
2020-10-15 23:07:53 +0900

    アセット追加と視点移動をマウス操作に変更

--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -1,45 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraMove : MonoBehaviour
 {
     GameObject player;
     Vector3 playerMove = Vector3.zero;
     Vector3 playerPos = Vector3.zero;
-    float rotateSpeed = 1f;
+    Vector2 lastMousePos = Vector2.zero;
+    float horizonRotateSpeed = 0.5f;
+    float varticalRotateSpeed = 0.1f;
+    float mouseMoveX = 0;
+    float mouseMoveY = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         playerMove = player.transform.position - playerPos;
+        lastMousePos = Input.mousePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //キー操作でカメラ方向転換
-        if (Input.GetKey(KeyCode.I))
-        {
-            transform.RotateAround(player.transform.position, this.transform.right, -1 * rotateSpeed);
-        }
-        if (Input.GetKey(KeyCode.L))
-        {
-            transform.RotateAround(player.transform.position, player.transform.up,rotateSpeed);
-        }
-        if (Input.GetKey(KeyCode.J))
-        {
-            transform.RotateAround(player.transform.position, player.transform.up, -1 * rotateSpeed);
-        }
-        if (Input.GetKey(KeyCode.K))
-        {
-            transform.RotateAround(player.transform.position, this.transform.right, rotateSpeed);
-        }
+        //マウス移動量初期化
+        mouseMoveX = 0;
+        mouseMoveY = 0;
+
+        //マウスのドラッグ量を変数に代入
+        mouseMoveX = Input.mousePosition.x - lastMousePos.x;
+        mouseMoveY = Input.mousePosition.y - lastMousePos.y;
+        lastMousePos = Input.mousePosition;
+
+        //マウス操作ででカメラ方向転換
+        transform.RotateAround(player.transform.position, player.transform.up, mouseMoveX * horizonRotateSpeed);
+        transform.RotateAround(player.transform.position, this.transform.right,-1 * mouseMoveY * varticalRotateSpeed);
+
+        Debug.Log(mouseMoveX);
 
         //カメラを方向転換なしで追従
         playerMove = player.transform.position - playerPos;
         playerPos = player.transform.position;
         this.transform.position += playerMove;
     }
+
 }
--- Assets/Script/JumpMove.cs
+++ Assets/Script/JumpMove.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class JumpMove : MonoBehaviour
 {
-    bool jumpSwitch = false;
-    bool jumping = false;
+    //bool jumpSwitch = false;
+    //bool jumping = false;
     float jumpForce = 350f;
     Rigidbody rb;
     Animator animator;
+    float interbal = 0;
+    AnimatorClipInfo nowAnm;
 
     void DoJump()//ジャンプ処理
     {
-        animator.SetTrigger("jumpTrigger");
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        nowAnm = animator.GetCurrentAnimatorClipInfo(0)[0];
+        if(nowAnm.clip.name != "BasicMotions@Jump01" && interbal > 0.5)
+        {
+            interbal = 0;
+            animator.SetTrigger("jumpTrigger");
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     // Start is called before the first frame update
@@ -26,24 +33,30 @@ public class JumpMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        interbal += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DoJump();
+        }
         //ジャンプ操作
-        if (Input.GetKeyDown(KeyCode.Space) && jumpSwitch == false && jumping == false)
+        /*if (Input.GetKeyDown(KeyCode.Space) && jumpSwitch == false && jumping == false)
         {
             jumpSwitch = true;
         }
         //Debug.Log(jumpSwitch);
         //Debug.Log(jumping);
 
-        if (Physics.Raycast(this.transform.position + new Vector3(0f,0.01f,0f), this.transform.TransformDirection(Vector3.down), 0.05f))
+        if (Physics.Raycast(this.transform.position + new Vector3(0f,0.1f,0f), this.transform.TransformDirection(Vector3.down), 0.13f))
         {
             jumping = false;
             animator.SetBool("groundBool", true);
             Debug.Log("着地");
-        }
+        }*/
 
     }
 
-    void FixedUpdate()
+    /*void FixedUpdate()
     {
         //ジャンプ判断
         if(jumpSwitch == true && jumping == false)
@@ -56,11 +69,11 @@ public class JumpMove : MonoBehaviour
         }
 
         //接地判定
-        /*if (Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.down), 0.1f))
+        if (Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.down), 0.1f))
         {
             jumping = false;
             animator.SetBool("groundBool", true);
             Debug.Log("着地");
-        }*/
-    }
+        }
+     }*/
 }
